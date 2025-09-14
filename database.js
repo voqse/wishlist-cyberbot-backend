46e3c47573bc57176bf53cbca0f4e82203a6009e
Denis Mainhardt <git@voqse.com>
2025-09-14 17:36:32 +0700

    Prepare database

---
+++ database.js
@@ -0,0 +1,49 @@
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+async function setupDatabase() {
+  const db = await open({
+    filename: './wishlist.db',
+    driver: sqlite3.Database
+  });
+
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY,
+      firstName TEXT NOT NULL,
+      lastName TEXT,
+      username TEXT,
+      languageCode TEXT,
+      isPremium INTEGER,
+      photoUrl TEXT
+    );
+
+    CREATE TABLE IF NOT EXISTS wishlists (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT NOT NULL,
+      createdBy INTEGER NOT NULL UNIQUE,
+      createdAt TEXT NOT NULL,
+      FOREIGN KEY (createdBy) REFERENCES users(id)
+    );
+
+    CREATE TABLE IF NOT EXISTS items (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      text TEXT NOT NULL,
+      links TEXT,
+      photos TEXT,
+      createdBy INTEGER NOT NULL,
+      createdAt TEXT NOT NULL,
+      reservedBy INTEGER,
+      reservedAt TEXT,
+      wishlistId INTEGER NOT NULL,
+      FOREIGN KEY (createdBy) REFERENCES users(id),
+      FOREIGN KEY (reservedBy) REFERENCES users(id),
+      FOREIGN KEY (wishlistId) REFERENCES wishlists(id)
+    );
+  `);
+
+  console.log('Database setup complete.');
+  return db;
+}
+
+export default setupDatabase;
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Happy developing ✨')
+import fastify from 'fastify';
+import setupDatabase from './database.js';
+
+const app = fastify({ logger: true });
+let db;
+
+app.get('/', async (request, reply) => {
+  return { hello: 'world' }
+});
+
+const start = async () => {
+  try {
+    db = await setupDatabase();
+    await app.listen({ port: 3000 });
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
